f9a7604fbf195a3ef1e49f4f556afdb917241687
damian-pereira <damian.pereira@alchemer.com>
2021-05-22 00:25:30 -0300

    Adds Playwright and Playwright.Tests projects Adds first test

---
+++ SmartSelectors/SmartSelectors.Playwright.Tests/FirstTest.cs
@@ -0,0 +1,27 @@
+﻿namespace SmartSelectors.Playwright.Tests
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+    using PlaywrightSharp;
+
+    public class FirstTest
+    {
+        [Test]
+        public static async Task Run()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions()
+            {
+                Headless = false
+            });
+            var page = await browser.NewPageAsync();
+
+            await page.GoToAsync("https://www.amazon.com/");
+            var handle = await page.QuerySelectorAsync("#nav-cart-count-container");
+            await handle.ClickAsync();
+
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ SmartSelectors/SmartSelectors.Playwright/ExtensionMethods.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SmartSelectors.Playwright
+{
+    public class ExtensionMethods
+    {
+    }
+}
